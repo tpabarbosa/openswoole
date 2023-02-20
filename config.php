4ff3efb746ef1aecb655b2d007dc2cb6fe9dc986
Tatiana Barbosa <tpabarbosa@yahoo.com.br>
2023-02-20 20:06:29 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,9 @@
 <?php
 
-define("DB_HOST", "sql10.freemysqlhosting.net");//"localhost");
-define("DB_PORT", 3306);
-define("DB_USER", "sql10598435");//"root");
-define("DB_PASSWORD", "xLagYX6GFX");//""root");
-define("DB_DATABASE", "sql10598435");//"carros");
 
-// define("DB_HOST", "localhost");
-// define("DB_PORT", 33061);
-// define("DB_USER", "root");
-// define("DB_PASSWORD", "root");
-// define("DB_DATABASE", "carros");
+
+define("DB_HOST", "localhost");
+define("DB_PORT", 33061);
+define("DB_USER", "root");
+define("DB_PASSWORD", "root");
+define("DB_DATABASE", "carros");
